5ec20f3448adda60b40c3df03aad89ebcaa0cd1c
Kent Clapp <kent.clapp@gmail.com>
2017-12-12 16:12:06 -0600

    Added 2 parmeters to re.sh

--- re.sh
+++ re.sh
@@ -1,9 +1,16 @@
 #!/bin/bash
-
+CONFIG="$1"
+COMMAND="$2"
+if [ $# -ne 2 ]
+then
+  echo "Usage: $0 {virtual-host} {restart|reload}"
+  echo "Reloads a target virtual host"
+  exit 1
+fi
 # Disable all virtual host configurations
-sudo a2dissite *
-sudo service apache2 restart
+sudo a2dissite "$CONFIG"
+sudo service apache2 "$COMMAND"
 
 # Enable all virtual host configuration
-sudo a2ensite *
-sudo service apache2 restart
+sudo a2ensite "$CONFIG"
+sudo service apache2 "$COMMAND"
